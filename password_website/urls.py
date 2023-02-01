7a4809cbbc43c448186acef8497fb37eaaf57b84
tarpetos <hevlichtaras@gmail.com>
2023-02-01 17:07:12 +0200

    Create start views

--- password_website/urls.py
+++ password_website/urls.py
@@ -16,6 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users import views as user_views
+
 urlpatterns = [
+    path('', user_views.users_list, name='home'),
+    path('usage/', user_views.usage_table, name='usage'),
+    path('countries/', user_views.countries_list, name='countries'),
+
+    path('users/add', user_views.users_add, name='users_add'),
+    path('users/<int:uid>/edit', user_views.users_edit, name='users_edit'),
+    path('users/<int:uid>/usage', user_views.users_usage, name='users_usage'),
+    path('users/<int:uid>/delete', user_views.users_delete, name='users_delete'),
+
+    path('countries/add', user_views.countries_add, name='countries_add'),
+    path('countries/<int:cid>/edit', user_views.countries_edit, name='countries_edit'),
+    path('countries/<int:cid>/usage', user_views.users_usage, name='countries_usage'),
+    path('countries/<int:cid>/delete', user_views.countries_delete, name='countries_delete'),
+
     path('admin/', admin.site.urls),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Views for users
+def users_list(request):
+    return render(request, 'users/users_list.html', {})
+
+
+def users_add(request):
+    return HttpResponse('<h1>User Add Form</h1>')
+
+
+def users_edit(request, uid):
+    return HttpResponse('<h1>Edit User %s</h1>' % uid)
+
+
+def users_usage(request, uid):
+    return HttpResponse('<h1>Usage For User %s</h1>' % uid)
+
+
+def users_delete(request, uid):
+    return HttpResponse('<h1>Delete User %s</h1>' % uid)
+
+
+# View for usage
+def usage_table(request):
+    return HttpResponse('<h1>Usage Table</h1>')
+
+
+# Views for countries
+def countries_list(request):
+    return HttpResponse('<h1>Countries Listing</h1>')
+
+
+def countries_add(requests):
+    return HttpResponse('<h1>Country Add Form</h1>')
+
+
+def countries_edit(requests, cid):
+    return HttpResponse('<h1>Edit Country %s</h1>' % cid)
+
+
+def countries_usage(request, cid):
+    return HttpResponse('<h1>Usage For User %s</h1>' % cid)
+
+
+def countries_delete(requests, cid):
+    return HttpResponse('<h1>Country Add Form %s</h1>' % cid)
